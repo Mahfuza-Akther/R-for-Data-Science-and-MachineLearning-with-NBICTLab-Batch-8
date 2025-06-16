6f0888cdf5014a955090dec1bdff2ab8f1ef4be7
Mahfuza-Akther <papry0704032@gmail.com>
2025-06-16 22:22:38 +0600

    Create  Histogram in R (Part-1).R
    Histogram in R

---
+++ RDSML-Day-13 👉 Histogram in R (Part-1)/ Histogram in R (Part-1).R
@@ -0,0 +1,43 @@
+#importing the dataset
+dataset = read.csv("data.csv")
+
+#Importing library
+library(ggplot2)
+
+#Basic histogram using ggplot2
+ggplot(dataset, aes(x = weight))+
+  geom_histogram()
+
+#Changing the width of bins and color
+ggplot(dataset, aes(x = weight))+
+  geom_histogram(binwidth = 1, color = "black")
+
+#Changing colors
+ggplot(dataset, aes(x = weight))+
+  geom_histogram(binwidth = 1, color = "black",
+                 fill = "lightblue")
+
+#Adding the mean line
+myPlot=ggplot(dataset, aes(x = weight))+
+  geom_histogram( color = "black",
+                 fill = "white")
+myPlot + geom_vline(aes(xintercept = mean(weight)),
+                    color = "blue", linetype = "dashed", size = 1)
+
+#Histogram with density plot
+ggplot(dataset, aes(x = weight))+
+  geom_histogram(aes(y = after_stat(density)), color = "black",
+                 fill = "white") +
+  geom_density(fill ="lightpink", alpha = .2)
+
+#Changing line color and fill color
+ggplot(dataset, aes(x = weight))+
+  geom_histogram(color = "blue",
+                 fill = "lightblue") 
+
+
+
+
+
+
+
