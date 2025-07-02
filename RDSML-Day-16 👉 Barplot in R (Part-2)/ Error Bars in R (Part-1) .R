373d64bdd5803cb071683e590308ac09e2dff106
Mahfuza-Akther <papry0704032@gmail.com>
2025-07-02 22:52:07 +0600

    Update  Error Bars in R (Part-1) .R

--- RDSML-Day-16 👉 Barplot in R (Part-2)/ Error Bars in R (Part-1) .R
+++ RDSML-Day-16 👉 Barplot in R (Part-2)/ Error Bars in R (Part-1) .R
@@ -16,3 +16,19 @@ data_summary = function(data, varname, groupnames){
 }
 
 d_summary = data_summary(tooth_data, var = "len", groupnames = c("supp", "dose"))
+
+# Converting 'dose' to a factor variable
+d_summary$dose = as.factor(d_summary$dose)
+class(d_summary$dose)
+
+#Standard deviation of the mean as error bar with explanation
+library(ggplot2)
+ggplot(d_summary, aes(x = dose, y = len, fill = supp)) +
+  geom_bar(stat = "identity", position = position_dodge()) +
+  geom_errorbar(aes(ymin = len - sd, ymax = len + sd),width = 0.2, position = position_dodge(.9)) +
+  theme_minimal()
+
+
+
+
+  
