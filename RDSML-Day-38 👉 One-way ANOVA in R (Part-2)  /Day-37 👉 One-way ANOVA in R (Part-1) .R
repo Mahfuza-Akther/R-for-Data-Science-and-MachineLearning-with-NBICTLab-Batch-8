1bba22959827d5239b7c76cb88263cb655900ecf
Mahfuza-Akther <papry0704032@gmail.com>
2025-09-11 11:37:06 +0600

    Day 38- One-way ANOVA (part-2)

--- RDSML-Day-38 👉 One-way ANOVA in R (Part-2)  /Day-37 👉 One-way ANOVA in R (Part-1) .R
+++ RDSML-Day-38 👉 One-way ANOVA in R (Part-2)  /Day-37 👉 One-way ANOVA in R (Part-1) .R
@@ -35,30 +35,10 @@ ggline(PlantGrowth,
 anova <- aov(weight ~ group, data = PlantGrowth)
 summary(anova)
 
-#Tukey multiple pairwise-comparisons
-TukeyHSD(anova)
 
-#Homogeneity of variances
-plot(anova, 1)
-library(car)
-leveneTest(weight ~ group, data = PlantGrowth)
 
-# ANOVA test with no Assumption of equal variances
-oneway.test(weight ~ group, data = PlantGrowth)
 
-#Pairwise t-tests with no assumption of equal variances
-pairwise.t.test(PlantGrowth$weight, PlantGrowth$group,
-                p.adjust.method = "BH", pool.sd = FALSE)
 
-# Checking for normal distribution
-plot(anova, 2)
 
-# Extracting the residuals
-aov_res <- residuals(object = anova)
 
-# Running the Shapiro-Wilk test
-shapiro.test(aov_res)
 
-kruskal.test(weight ~ group, data = PlantGrowth)
-pairwise.wilcox.test(PlantGrowth$weight, PlantGrowth$group,
-                     p.adjust.method = "BH")
