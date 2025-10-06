2a19f66d78d1639d55a32b60730bcd77d9244372
Mahfuza-Akther <papry0704032@gmail.com>
2025-10-06 22:48:49 +0600

    RDSML-Day-44-Chi-Square Test (Part-1)

---
+++ RDSML-Day-44 👉 Chi-Square Test of Independence in R (Part-1) /RDSML-Day-44 -Chi-Square Test of Independence in R (Part-1) .R
@@ -0,0 +1,40 @@
+# Importing the data set
+housetasks <- read.delim("housetasks.txt", row.names = 1)
+
+# What is the chi-square test of independence?
+# = For frequency table analysis 
+#Contingency table can e visualized using the function ballonplot 
+#(This function draws a graphical matrix where each cell
+# contains a dot whose reflects the relative magnitude 
+#of the correspondig component)
+
+install.packages("gplots")
+library("gplots")
+data_table <- as.table(as.matrix(housetasks))
+balloonplot(t(data_table), main = "Housetasks Visualization",
+            label = F, show.margins = F,
+            xlab = "", ylab = "")
+
+# Its also possible to visualize the cobtingency table
+# as a mosic plot, this can be done using the function 
+# mosicplot()from the built-in R package graphics
+library("graphics")
+mosaicplot(data_table, shade = T, las = 2, main = "Housetasks")
+# Blue color indicates that the observed value and the 
+#red color indicate the observed valoue is lower than expected value 
+#value
+
+# Computing the chi-square test in R
+chisq <- chisq.test(housetasks)
+chisq
+
+# Observed counts and expected counts housetasks
+# Observed counts 
+chisq$observed
+# Expected counts
+round(chisq$expected, 2)
+
+
+
+
+
